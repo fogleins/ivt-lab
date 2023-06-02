6e3d236162fca55c24836513b9cd47380cf13414
fogleins <fogleins@hotmail.hu>
2023-06-02 11:25:08 +0200

    Add unit test and JaCoCo code coverage

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -10,20 +10,29 @@ public class GT4500Test {
 
   private GT4500 ship;
 
+  private TorpedoStore ts1;
+  private TorpedoStore ts2;
+
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    ts1 = mock(TorpedoStore.class);
+    ts2 = mock(TorpedoStore.class);
+    this.ship = new GT4500(ts1, ts2);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(ts1.fire(1)).thenReturn(true);
+    when(ts1.isEmpty()).thenReturn(false);
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+
+    verify(ts1, times(1)).fire(1);
+    verify(ts2, times(0)).fire(1);
   }
 
   @Test
